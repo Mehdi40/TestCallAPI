2d089bb450f9c7bffe398041e3da2680664096f9
Mehdi Lafenetre <mehdi.lafenetre@signactif.com>
2018-02-07 17:08:06 +0100

    :white_check_mark: Adding the tests

---
+++ test/helpers/request.test.js
@@ -0,0 +1,45 @@
+import chai from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+chai.use(sinonChai)
+const should = chai.should()
+import 'babel-polyfill'
+
+
+import verifyRequest from '../../helpers/request'
+import {
+  fakeRequestWithoutFaces,
+  fakeRequestWithoutPicture,
+  fakeRequestWithOneFace,
+  fakeRequestWithMultipleFaces,
+} from '../mockup/requests'
+
+describe('[Request Helper] - verifyRequest', async () => {
+  it('should return a 404 error message if no picture is provided', async () => {
+    const p = await verifyRequest({body: fakeRequestWithoutPicture})
+
+    p.should.contain({ message: 'Oops ! Something went wrong during the process.' })
+    p.should.contain({ code: 404 })
+  })
+
+  it('should return a 404 error message if a picture with no faces is provided', async () => {
+    const p = await verifyRequest({body: fakeRequestWithoutFaces })
+    
+    p.should.contain({ message: 'No faces found on this image.' })
+    p.should.contain({ code: 404 })
+  })
+
+  it('should return a 200 success message if a picture with one face is provided', async () => {
+    const p = await verifyRequest({body: fakeRequestWithOneFace })
+
+    p.should.contain({ message: 'Success ! A face has been found on this image.' })
+    p.should.contain({ code: 200 })
+  })
+
+  it('should return a 200 success message if a picture with many faces is provided', async () => {
+    const p = await verifyRequest({body: fakeRequestWithMultipleFaces })
+
+    p.should.contain({ message: 'Success ! Faces have been found on this image.' })
+    p.should.contain({ code: 200 })
+  })
+})
---
+++ test/mockup/requests.js
@@ -0,0 +1,30 @@
+import config from '../../config/config'
+
+const fakeRequestWithoutPicture = {
+  username: 'Oui',
+  mail: 'oui@oui.oui',
+  password: 'oui',
+}
+
+const fakeRequestWithoutFaces = {
+  username: 'Oui',
+  mail: 'oui@oui.oui',
+  password: 'oui',
+  picture: config.pictureWithNoFace
+}
+
+const fakeRequestWithOneFace = {
+  username: 'Oui',
+  mail: 'oui@oui.oui',
+  password: 'oui',
+  picture: config.pictureWithOneFace
+}
+
+const fakeRequestWithMultipleFaces = {
+  username: 'Oui',
+  mail: 'oui@oui.oui',
+  password: 'oui',
+  picture: config.pictureWithManyFaces
+}
+
+module.exports = { fakeRequestWithoutPicture, fakeRequestWithoutFaces, fakeRequestWithOneFace, fakeRequestWithMultipleFaces }
